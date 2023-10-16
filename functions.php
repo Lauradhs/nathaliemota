3708f21fcb70ac6a40f9ff17e48948cc7dbc8862
Laura Dhiers <laura-dh@hotmail.fr>
2023-10-16 17:47:56 +0200

    lien css

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
 function theme_enqueue_style()
 {
-    wp_enqueue_style('parent-style', get_stylesheet_uri(), array(), '1.0');
+    wp_enqueue_style('mon-style', get_stylesheet_directory_uri() . '/styles.css', array(), '1.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_style');
 
 
+?>
